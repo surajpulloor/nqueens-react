e83ebd264b5ba2cb919b6b88f4802f073aeda2a7
Suraj Pulloor <thegamer600@gmail.com>
2021-06-05 12:18:24 +0530

    Changes App component into a class based component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 import '../styles/App.css';
+import ChessBoard from './ChessBoard';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="container">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+
+    super(props);
+    this.state = {};
+
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+  
+        <div className="row">
+          <div className="col-md-6">
+  
+            <ChessBoard />
+          </div>
+          <div className="col-md-6">
+            <form>
+              <input type="text" placeholder="N" className="form-control" />
+              <input type="button" value="Solve" className="btn btn-primary" />
+              <input type="button" value="Prev" className="btn btn-primary" />
+              <input type="button" value="Next" className="btn btn-primary" />
+            </form>
+          </div>
+        </div>
+  
+        
+      </div>
+    );
+
+  }
 }
+
 
 export default App;
