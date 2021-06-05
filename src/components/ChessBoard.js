9d2d659c8cdcde4b73d374c91c32bdc225c84d51
Suraj Pulloor <thegamer600@gmail.com>
2021-06-05 13:50:45 +0530

    ChessBoard component implemented styles added as well

---
+++ src/components/ChessBoard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChessQueen } from '@fortawesome/free-solid-svg-icons';
+
+import '../styles/ChessBoard.css';
+
+function ChessBoard(props) {
+
+    return (
+
+        <div className="solutions">
+
+            {
+                props.solutions !== null && 
+                props.index !== -1 && 
+                props.n !== '' && 
+                props.solutions[props.index].length === parseInt(props.n)
+
+                ?
+
+                <table className="chess-board">
+
+                    { 
+                        props.solutions[props.index].map((solutionIndex, index) => (
+                            <tr>
+                                {
+                                    new Array(parseInt(props.n)).fill(true).map((value, index2) => (
+                                        <td>
+                                            {
+                                                solutionIndex === index2 ? <FontAwesomeIcon icon={faChessQueen} /> : ' '
+                                            }
+                                        </td>
+                                    ))
+                                }
+                            </tr>
+
+                        ))
+                    }
+
+                </table>
+
+                :
+                <h1>Chess Component</h1>
+            }
+
+
+        </div>
+
+        
+    );
+
+}
+
+export default ChessBoard;
