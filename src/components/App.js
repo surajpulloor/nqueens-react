b3ac1c417979ca481e1294ec85437b302b463e00
Suraj Pulloor <thegamer600@gmail.com>
2021-06-05 13:51:16 +0530

    Interagted ChessBoard component with App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,46 @@
 import '../styles/App.css';
 import ChessBoard from './ChessBoard';
 import React, {Component} from 'react';
+import Nqueens from "../utility/Nqueens";
 
 class App extends Component {
   constructor(props) {
 
     super(props);
-    this.state = {};
+    this.state = {
+      n: '',
+      index: -1,
+      solutions: null
+    };
 
+  }
+
+  solve = (e) => {
+
+    const n = parseInt(this.state.n);
+
+    const nqueens = new Nqueens(n);
+
+    this.setState({
+      solutions: nqueens.solve(0),
+      index: -1
+    });
+
+  }
+
+
+  prev = (e) => {
+
+    this.setState((prevState) => ({
+      index: prevState.index - 1
+    }));
+
+  }
+
+  next = (e) => {
+    this.setState((prevState) => ({
+      index: prevState.index + 1
+    }));
   }
 
   render() {
@@ -18,14 +51,14 @@ class App extends Component {
         <div className="row">
           <div className="col-md-6">
   
-            <ChessBoard />
+            <ChessBoard solutions={this.state.solutions} index={this.state.index + 1} n={this.state.n} />
           </div>
           <div className="col-md-6">
             <form>
-              <input type="text" placeholder="N" className="form-control" />
-              <input type="button" value="Solve" className="btn btn-primary" />
-              <input type="button" value="Prev" className="btn btn-primary" />
-              <input type="button" value="Next" className="btn btn-primary" />
+              <input type="text" placeholder="N" className="form-control" value={this.state.n} onChange={(e) => this.setState({n: e.target.value})} />
+              <input type="button" value="Solve" className="btn btn-primary" disabled={this.state.n === ''} onClick={this.solve} />
+              <input type="button" value="Prev" className="btn btn-primary" disabled={this.state.index === -1} onClick={this.prev} />
+              <input type="button" value="Next" className="btn btn-primary" disabled={this.state.solutions && this.state.index > this.state.solutions.length} onClick={this.next} />
             </form>
           </div>
         </div>
